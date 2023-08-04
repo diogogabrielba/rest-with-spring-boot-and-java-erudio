b88bed88b3aabf15aec3922dcd7d67a33686163c
diogogabrielba <diogo.gabriel259@gmail.com>
2023-08-04 16:13:01 -0300

    First steps in java with Spring Boot 3

---
+++ rest-with-spring-boot-and-java/rest-with-spring-boot-and-java/src/main/java/br/com/curso/udemy/RestStartupApplication.java
@@ -0,0 +1,13 @@
+package br.com.curso.udemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestStartupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestStartupApplication.class, args);
+	}
+
+}
